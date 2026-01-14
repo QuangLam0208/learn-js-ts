4ee3084f5ada16db2fed50a3a4856e327f9bb1c0
Quang Lam <laulua1223@outlook.com>
2026-01-14 09:52:42 +0700

    data type: object & array

---
+++ src/vid19/vid19.js
@@ -0,0 +1,15 @@
+
+console.log("Vid 19");
+
+// object : key - value
+const person = {
+    name: "Persy",
+    age: 20,
+    address: "Ho Chi Minh"
+};
+
+console.log(person);
+
+// array
+const scores = [10, 3, 4, 2, 7];
+console.log(scores);
