b0c0f7cc44a8fb83a548a30918349743f6cf6edd
Quang Lam <laulua1223@outlook.com>
2026-01-14 10:26:20 +0700

    using template literals

---
+++ src/vid22/vid22.js
@@ -0,0 +1,11 @@
+console.log("Vid 22");
+
+const name = "Quang Lam";
+const age = 20;
+
+const introduction = "My name is " + name + " and I am " + age + " years old.";
+console.log(introduction);
+
+// using template literals
+const intro = `My name is "${name}" and I am ${age} years old.`;
+console.log(intro);
