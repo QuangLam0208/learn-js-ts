8af84bb04203918c70e150b955b982f6c70be428
Quang Lam <laulua1223@outlook.com>
2026-01-14 09:46:25 +0700

    primitives data types

---
+++ src/vid18/vid18.js
@@ -0,0 +1,23 @@
+
+// number
+const myAge = 25;
+const myScore = 9.1;
+
+console.log("vid 18");
+
+// string
+const name = "Lam dep trai";
+const name1 = 'persy'; // not use
+console.log(name);
+
+// boolean : true/false
+const isHandsome = true;
+console.log(isHandsome);
+
+// undefined
+let bla;
+console.log(bla);
+
+// null
+let testNull = null;
+console.log(testNull);
