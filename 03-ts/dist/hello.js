cca6b112ff060935a2f60588f2f65e217c60b075
Luong Quang Lam <laulua1223@gmail.com>
2026-07-18 03:36:24 +0700

    beginning with ts

---
+++ 03-ts/dist/hello.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("Hello world");
+console.log("Test watching for file");
+console.log("Demo running with ts from server");
---
+++ 03-ts/src/hello.ts
@@ -0,0 +1,3 @@
+console.log("Hello world")
+console.log("Test watching for file")
+console.log("Demo running with ts from server")
